cedf3e96fe7b2c1f720c798c17a591054203f27e
savayer <sava0094@gmail.com>
2020-05-21 22:02:36 +0800

    finished

--- src/js/index.ts
+++ src/js/index.ts
@@ -5,7 +5,8 @@ import app from './modules/Dom'
 document.querySelector('#start_game').addEventListener('click', () => {
     const word: string = getRandomWord(data.words)
     
-    app.clear()
+    app.init()
     app.seperateByWords(word)
     app.generateAlphabet()
+    app.game = true
 })
--- src/js/modules/Dom.ts
+++ src/js/modules/Dom.ts
@@ -2,7 +2,9 @@ class Dom {
     private parent: HTMLElement;
     private puzzle: HTMLElement;
     private alphabet: HTMLElement;
-    
+    private word: string;
+    public game: boolean = true;
+
     constructor(selector: string) {
         this.parent = document.querySelector(selector)
         this.puzzle = this.parent.querySelector('.word')
@@ -10,6 +12,7 @@ class Dom {
     }
 
     public seperateByWords(word: string): void {
+        this.word = word
         word.split('').forEach(letter => {
             const span: HTMLSpanElement = document.createElement('span')
             if (letter === ' ') {
@@ -30,13 +33,57 @@ class Dom {
             const button: HTMLButtonElement = document.createElement('button')
             
             button.classList.add('letter')
+            button.dataset.button_id = letterStart.toString()
             button.innerText = String.fromCharCode(letterStart).toUpperCase()
             
+            button.addEventListener('click', () => {
+                if (!this.game) return false;
+                const id: string = button.dataset.button_id
+                this.findLettersInGuessedWord(id)
+
+                button.classList.add('disabled')
+            })
+
             this.alphabet.appendChild(button)
         }
     }
 
-    public clear(): void {
+    private findLettersInGuessedWord(id: string): void {
+        const spans: NodeList = document.querySelectorAll(`[data-id="${id}"]`)
+        if (spans.length > 0) {
+            spans.forEach(function(letter: HTMLSpanElement) {
+                const letterCode: string = letter.dataset.id
+                letter.classList.add('guessed')
+                letter.innerHTML = String.fromCharCode(+letterCode)
+            })
+            this.checkWin()
+        } else {
+            this.nextStepOfHanging()
+        }
+    }
+
+    private nextStepOfHanging(): void {
+        const img = document.querySelector('.hangman.active')
+        const nextImg = img.nextElementSibling
+
+        img.classList.remove('active')
+        nextImg.classList.add('active')
+        if (nextImg.classList.contains('hangman--7')) {
+            this.game = false
+            alert('Ты проиграл! Загаданное слово: ' + this.word)
+        }
+    }
+
+    private checkWin(): void {
+        if (document.querySelectorAll('.guessed').length === this.word.length) {
+            this.game = false
+            alert('Ты выиграл! Нажимай "Начать игру"')
+        }
+    }
+
+    public init(): void {
+        document.querySelector('.hangman.active').classList.remove('active')
+        document.querySelector('.hangman--1').classList.add('active')
         this.alphabet.innerHTML = ''
         this.puzzle.innerHTML = ''
     }
